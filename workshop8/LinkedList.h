ea25ae11569922b8c6e0a853ae26525d1927baca
Sarah Telford <84111693+sarahtelford@users.noreply.github.com>
2022-10-26 13:45:00 +1030

    Add files via upload

---
+++ workshop8/LinkedList.h
@@ -0,0 +1,12 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H 
+
+class LinkedList
+{
+public:
+	LinkedList();
+	Node * head;
+	~LinkedList();
+	
+};
+#endif
---
+++ workshop8/Node.cpp
@@ -0,0 +1,16 @@
+#ifndef NODE_H
+#define NODE_H 
+
+class Node
+{
+public:
+	Node();
+
+	~Node();
+
+protected: 
+	int data;
+
+	
+};
+#endif
